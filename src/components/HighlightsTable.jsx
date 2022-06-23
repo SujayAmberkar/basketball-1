9f16f3d568cb57a69e01bc8ba261141e749c0885
SujayAmberkar <38327075+SujayAmberkar@users.noreply.github.com>
2022-06-24 02:15:36 +0530

    Update HighlightsTable.jsx

--- src/components/HighlightsTable.jsx
+++ src/components/HighlightsTable.jsx
@@ -6,8 +6,6 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { TextField
- } from '@mui/material';
 
 
 const data=[
